e5ea1b78863e3a869a6cad7e1d436f0bc50e767d
Flamewind97 <chelew@student.ethz.ch>
2023-04-20 10:31:30 +0200

    refactor part 1 - private can run

--- ecc_go/chaincode/singleKVS.go
+++ ecc_go/chaincode/singleKVS.go
@@ -6,26 +6,26 @@ SPDX-License-Identifier: Apache-2.0
 
 package chaincode
 
-import (
-	"github.com/hyperledger/fabric-chaincode-go/shim"
-	"github.com/hyperledger/fabric-private-chaincode/ecc/chaincode"
-	"github.com/hyperledger/fabric-private-chaincode/ecc/chaincode/ercc"
-	"github.com/hyperledger/fabric-private-chaincode/ecc_go/chaincode/enclave_go"
-	"github.com/hyperledger/fabric-private-chaincode/internal/endorsement"
-	"github.com/hyperledger/fabric/common/flogging"
-)
+// import (
+// 	"github.com/hyperledger/fabric-chaincode-go/shim"
+// 	"github.com/hyperledger/fabric-private-chaincode/ecc/chaincode"
+// 	"github.com/hyperledger/fabric-private-chaincode/ecc/chaincode/ercc"
+// 	"github.com/hyperledger/fabric-private-chaincode/ecc_go/chaincode/enclave_go"
+// 	"github.com/hyperledger/fabric-private-chaincode/internal/endorsement"
+// 	"github.com/hyperledger/fabric/common/flogging"
+// )
 
-var logger = flogging.MustGetLogger("enclave_go")
+// var logger = flogging.MustGetLogger("enclave_go")
 
-func NewSkvsChaincode(cc shim.Chaincode) *chaincode.EnclaveChaincode {
-	ecc := &chaincode.EnclaveChaincode{
-		Enclave:   enclave_go.NewSkvsStub(cc),
-		Validator: endorsement.NewValidator(),
-		Extractor: &chaincode.ExtractorImpl{},
-		Ercc:      &ercc.StubImpl{},
-	}
-	return ecc
-}
+// func NewSkvsChaincode(cc shim.Chaincode) *chaincode.EnclaveChaincode {
+// 	ecc := &chaincode.EnclaveChaincode{
+// 		Enclave:   enclave_go.NewSkvsStub(cc),
+// 		Validator: endorsement.NewValidator(),
+// 		Extractor: &chaincode.ExtractorImpl{},
+// 		Ercc:      &ercc.StubImpl{},
+// 	}
+// 	return ecc
+// }
 
 // type skvsStub struct {
 // 	*enclave_go.EnclaveStub
