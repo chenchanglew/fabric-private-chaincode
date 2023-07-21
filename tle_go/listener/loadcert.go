047a490ab6e8e67491eac1a267dee6d7edb115c4
Flamewind97 <chelew@student.ethz.ch>
2023-07-21 18:46:01 +0200

    add block listener
    Signed-off-by: chenchanglew <chelew@student.ethz.ch>

---
+++ tle_go/listener/loadcert.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+
+	"google.golang.org/grpc/credentials"
+)
+
+func loadTLSCredentials() (credentials.TransportCredentials, error) {
+	// Load certificate of the CA who signed server's certificate
+	pemServerCA, err := ioutil.ReadFile("ca/ca-certs.pem")
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		return nil, fmt.Errorf("failed to add server CA's certificate")
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		RootCAs: certPool,
+	}
+
+	return credentials.NewTLS(config), nil
+}
